c0d9c94c2ba15c16b71e92aaaa69c3c3e5ddc06c
CarmenZak <186872558+CarmenZak@users.noreply.github.com>
2025-01-30 16:00:37 +0000

    Add header and footer

---
+++ src/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+
+export default function Footer () {
+    return (
+        <div> 
+        <p>🎵 Built with React by Team CNZ 🎵</p>
+        
+        </div>
+        );
+    }
---
+++ src/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './header.css'
+
+
+export default function Header() {
+    return (
+        <div>
+        <h2> 🎶🎶 Play that chooon!! 🎶🎶</h2>
+        <p> Get ready for the morning squat session!! Add a song suggestion to the form below...</p>
+        </div>
+    )
+}
