13e6ec614365ad4e2f7c99bcf55b16df67625bd6
Nick Goh <nickygoh8@gmail.com>
2025-01-30 18:29:32 +0000

    adds data.js and imports to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
-import './App.css'
-import VideoPlayerCard from './components/videoplayer/videoplayer'
-import Header from './header/header'
-import Footer from './footer/footer'
+import './App.css';
+import VideoPlayerCard from './components/videoplayer/videoplayer';
+import Header from './header/header';
+import Footer from './footer/footer';
+import { data } from './data/data';
 
 function App() {
+
+  // initialize state of videos with data imported from data.js
+  const [videos, setVideos] = useState(data);
 
   return (
     <>
---
+++ src/data/data.js
@@ -0,0 +1,16 @@
+export const data = [
+    {
+        id: 1,
+        title: 'Total Eclipse of the Heart',
+        username: 'Nick G',
+        url: 'https://www.youtube.com/watch?v=lcOxhH8N3Bo',
+        description: 'Stretch through the slow start and wait for the beat to drop!'
+    },
+    {
+        id: 2,
+        title: 'Bye Bye Bye',
+        username: 'Sumeya',
+        url: 'https://www.youtube.com/watch?v=lcOxhH8N3Bo',
+        description: 'This is a video 2'
+    },
+];
